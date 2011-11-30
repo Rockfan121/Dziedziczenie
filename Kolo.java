72644606aa62ba055afed03ec47cb0bef6655daa
Karolina Mura <k.mura121@gmail.com>
2011-11-30 17:34:16 +0100

    Initial commit

---
+++ Kolo.java
@@ -0,0 +1,20 @@
+public class Kolo extends Figura
+{
+	private double x;
+	private double y;
+	private double z;
+	
+	Kolo (double x, double y, double z)
+	{
+		this.x = x;
+		this.y = y;
+		this.z = z;
+		
+	}
+	
+	@Override
+	public void rysuj()
+	{
+		System.out.println('Rysujemy kolo o srodku:' + d +)
+	}
+}
