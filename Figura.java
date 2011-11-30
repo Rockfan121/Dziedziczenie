29bb909d5dcd058bb2a3259097c4b737c0a3e55e
Karolina Mura <k.mura121@gmail.com>
2011-11-30 17:29:03 +0100

    Initial commit

---
+++ Figura.java
@@ -0,0 +1,13 @@
+public class Figura
+{
+	public abstract void rysuj (); //nie ma definicji, interfejs
+	
+	
+	public static void main(String[] args) 
+	{
+		Figura a = new Kolo (new Punkt )
+		a
+		b = new Prostokat 
+	}
+	
+}
